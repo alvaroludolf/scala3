b6800b2ff74aae2be37cb4e77ccdc58f469603e5
Alvaro Ludolf Ribeiro <alvaroludolf@gmail.com>
2022-08-04 05:38:50 +0200

    exe 4

--- src/main/scala/quickcheck/HeapProperties.scala
+++ src/main/scala/quickcheck/HeapProperties.scala
@@ -5,90 +5,97 @@ import Arbitrary.*
 import Gen.*
 import Prop.*
 
+import java.lang.Math.max
+
 trait HeapProperties(val heapInterface: HeapInterface):
 
   // Import all the operations of the `HeapInterface` (e.g., `empty`
   // `insert`, etc.)
+
   import heapInterface.*
 
   val minOfTwo: (String, Prop) =
     "the minimum of a heap of two elements should be the smallest of the two elements" ->
-    forAll { (x1: Int, x2: Int) =>
-      val heap = insert(x2, insert(x1, empty))
-      val min = if x1 <= x2 then x1 else x2
-      findMin(heap) == min
-    }
+      forAll { (x1: Int, x2: Int) =>
+        val heap = insert(x2, insert(x1, empty))
+        val min = if x1 <= x2 then x1 else x2
+        findMin(heap) == min
+      }
 
   val deleteMinOfOne: (String, Prop) =
     "delete minumum of heap of one element should return an empty heap" ->
-    forAll { (x: Int) =>
-      // create a heap with exactly one element, `x`
-      val heap1: List[Node] = ???
-      // delete the minimal element from it
-      val heap0: List[Node] = ???
-      // check that heap0 is empty
-      isEmpty(heap0)
-    }
+      forAll { (x: Int) =>
+        // create a heap with exactly one element, `x`
+        val heap1: List[Node] = insert(x, empty)
+        // delete the minimal element from it
+        val heap0: List[Node] = deleteMin(heap1)
+        // check that heap0 is empty
+        isEmpty(heap0)
+      }
 
   val insertMinAndGetMin: (String, Prop) =
     "inserting the minimal element and then finding it should return the same minimal element" ->
-    forAll(generatedHeap.suchThat(heap => !isEmpty(heap))) { (heap: List[Node]) =>
-      // find the miniminal element of the heap
-      // (you don’t need to handle the case of empty heaps because it has been excluded from the heap generator)
-      val min: Int = ???
-      // insert the minimal element to the heap
-      val updatedHeap: List[Node] = ???
-      // find the minimal element of the updated heap should return the same minimal element
-      findMin(updatedHeap) == min
-    }
+      forAll(generatedHeap.suchThat(heap => !isEmpty(heap))) { (heap: List[Node]) =>
+        // find the miniminal element of the heap
+        // (you don’t need to handle the case of empty heaps because it has been excluded from the heap generator)
+        val min: Int = findMin(heap)
+        // insert the minimal element to the heap
+        val updatedHeap: List[Node] = insert(min, heap)
+        // find the minimal element of the updated heap should return the same minimal element
+        findMin(updatedHeap) == min
+      }
 
   val deleteAllProducesSortedList: (String, Prop) =
     // recursively traverse the heap
     def check(heap: List[Node]): Boolean =
-      // if the heap is empty, or if it has just one element, we have
-      // successfully finished our checks
+    // if the heap is empty, or if it has just one element, we have
+    // successfully finished our checks
       if isEmpty(heap) || isEmpty(deleteMin(heap)) then
         true
       else
         // find the minimal element
-        val x1: Int = ???
+        val x1: Int = findMin(heap)
         // delete the minimal element of `heap`
-        val heap2: List[Node] = ???
+        val heap2: List[Node] = deleteMin(heap)
         // find the minimal element in `heap2`
-        val x2: Int = ???
+        val x2: Int = findMin(heap2)
         // check that the deleted element is less than or equal to the
         // minimal element of the remaining heap, and that the remaining
         // heap verifies the same property (by recursively calling `check`)
-        val checked: Boolean = ???
+        val checked: Boolean = x1 <= x2 && check(heap2)
         checked
     // check arbitrary heaps
     "continually finding and deleting the minimal element of a heap should return a sorted sequence" ->
-    forAll { (heap: List[Node]) =>
-      check(heap)
-    }
+      forAll { (heap: List[Node]) =>
+        check(heap)
+      }
 
   val meldingSmallHeaps: (String, Prop) =
     "melding a heap containing two low values with a heap containing two high values" ->
-    forAll { (x: Int, y: Int) =>
-      // create two heaps:
-      // - the first has two duplicate elements inserted, where both are equal to the
-      //   highest value among `x` and `y`
-      // - the second also has two duplicate elements insterted, where both are equal
-      //   to the lowest value among `x` and `y`
-      // finally, meld both heaps.
-      val meldedHeap: List[Node] = ???
-      // check that deleting the minimal element twice in a row from the melded heap,
-      // and then finding the minimal element in the resulting heap returns the
-      // highest value
-      val deleteTwoMinAndFindMin: Boolean =
-        ???
-      // check that inserting the lowest value to the melded heap, and then
-      // finding the minimal element returns the lowest value
-      val insertMinAndFindMin: Boolean =
-        ???
-      // check that both conditions are fulfilled
-      deleteTwoMinAndFindMin && insertMinAndFindMin
-    }
+      forAll { (x: Int, y: Int) =>
+        // create two heaps:
+        // - the first has two duplicate elements inserted, where both are equal to the
+        //   highest value among `x` and `y`
+        // - the second also has two duplicate elements insterted, where both are equal
+        //   to the lowest value among `x` and `y`
+        // finally, meld both heaps.
+        val min = Math.min(x, y)
+        val max = Math.max(x, y)
+        val heap1 = insert(max, insert(max, empty))
+        val heap2 = insert(min, insert(min, empty))
+        var meldedHeap: List[Node] = meld(heap1, heap2)
+        // check that deleting the minimal element twice in a row from the melded heap,
+        // and then finding the minimal element in the resulting heap returns the
+        // highest value
+        meldedHeap = deleteMin(deleteMin(meldedHeap))
+        val deleteTwoMinAndFindMin: Boolean = max == findMin(meldedHeap)
+        // check that inserting the lowest value to the melded heap, and then
+        // finding the minimal element returns the lowest value
+        meldedHeap = insert(min, meldedHeap)
+        val insertMinAndFindMin: Boolean = min == findMin(meldedHeap)
+        // check that both conditions are fulfilled
+        deleteTwoMinAndFindMin && insertMinAndFindMin
+      }
 
   // Given two arbitrary heaps, and the heap that results from melding
   // them together, finding the minimum of the melded heap should return
@@ -115,13 +122,37 @@ trait HeapProperties(val heapInterface: HeapInterface):
   //  4. all the other cases (which should not happen in correct heap
   //     implementations).
   val meldingHeaps: (String, Prop) =
+    def check(heap1: List[Node], heap2: List[Node], heapm: List[Node]): Boolean =
+      if !isEmpty(heap1) then
+        if !isEmpty(heap2) then
+          val minm: Int = findMin(heapm)
+          val min1: Int = findMin(heap1)
+          val min2: Int = findMin(heap2)
+          if (min1 <= min2) then
+            minm == min1 && check(deleteMin(heap1), heap2, deleteMin(heapm))
+          else
+            minm == min2 && check(heap1, deleteMin(heap2), deleteMin(heapm))
+        else
+          val minm: Int = findMin(heapm)
+          val min1: Int = findMin(heap1)
+          minm == min1 && check(deleteMin(heap1), heap2, deleteMin(heapm))
+      else
+        if !isEmpty(heap2) then
+          val minm: Int = findMin(heapm)
+          val min2: Int = findMin(heap2)
+          minm == min2 && check(heap1, deleteMin(heap2), deleteMin(heapm))
+        else
+          true
+
     "finding the minimum of melding any two heaps should return the minimum of one or the other of the source heaps" ->
     forAll { (heap1: List[Node], heap2: List[Node]) =>
-      ???
+      val heapm = meld(heap1, heap2)
+      check(heap1, heap2, heapm)
     }
 
   // Random heap generator (used by Scalacheck)
   given generatedHeap: Gen[List[Node]]
+
   given Arbitrary[List[Node]] = Arbitrary(generatedHeap)
 
 end HeapProperties
